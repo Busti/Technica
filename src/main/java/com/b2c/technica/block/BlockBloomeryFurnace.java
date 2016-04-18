989156ebae4bc0801c22eacd626fbe793357d109
Alexander Krause <alexander.krause3@web.de>
2016-04-19 01:20:48 +0200

    TileBloomeryFurnace

--- src/main/java/com/b2c/technica/block/BlockBloomeryFurnace.java
+++ src/main/java/com/b2c/technica/block/BlockBloomeryFurnace.java
@@ -1,8 +1,12 @@
 package com.b2c.technica.block;
 
+import com.b2c.technica.block.tileentity.TileBloomeryFurnace;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 /**
  * Created by akrau on 19.04.2016.
@@ -10,5 +14,11 @@ import net.minecraft.block.material.Material;
 public class BlockBloomeryFurnace extends Block{
     public BlockBloomeryFurnace() {
         super(Material.clay, MapColor.grayColor);
+    }
+
+    @Override
+    public TileEntity createTileEntity(World world, IBlockState state) {
+        return new TileBloomeryFurnace();
+
     }
 }
---
+++ src/main/java/com/b2c/technica/block/tileentity/TileBloomeryFurnace.java
@@ -0,0 +1,10 @@
+package com.b2c.technica.block.tileentity;
+
+import net.minecraft.tileentity.TileEntity;
+
+/**
+ * Created by akrau on 19.04.2016.
+ */
+public class TileBloomeryFurnace extends TileEntity{
+
+}
