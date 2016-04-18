2e9f6858352cb6a1dc859f8831966b4eab3cc8cb
Busti <mbust@live.de>
2016-04-18 16:29:39 +0200

    Updated mcMod.info

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -12,8 +12,7 @@ public class ExampleMod
     public static final String VERSION = "1.0";
     
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
 		// some example code
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
     }
