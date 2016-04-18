533f78cf72e2d1e62104d4e73a19d45e97b62948
Busti <mbust@live.de>
2016-04-18 16:41:47 +0200

    Created main class

--- src/main/java/com/b2c/technica/ExampleMod.java
+++ src/main/java/com/b2c/technica/ExampleMod.java
@@ -1,6 +1,5 @@
-package com.example.examplemod;
+package com.b2c.technica;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -8,12 +7,11 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
+    public static final String MODID = "technica";
+    public static final String VERSION = "0.3.1";
+
     @EventHandler
     public void init(FMLInitializationEvent event) {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+
     }
 }
