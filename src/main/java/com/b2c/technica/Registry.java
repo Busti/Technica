0efbc5e75e226eeba66cd321a1a99a5ff6e93008
Busti <mbust@live.de>
2016-04-19 13:16:44 +0200

    Updated gradle wrapper to java 1.8

--- src/main/java/com/b2c/technica/Registry.java
+++ src/main/java/com/b2c/technica/Registry.java
@@ -3,7 +3,11 @@ package com.b2c.technica;
 import com.b2c.technica.block.BlockBloomeryFurnace;
 import com.b2c.technica.block.BlockTMeta;
 import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import javax.annotation.Nullable;
+import java.util.function.Function;
 
 public class Registry {
     //Blocks
@@ -15,7 +19,23 @@ public class Registry {
         blockOre = new BlockTMeta();
         blockBloomeryFurnce = new BlockBloomeryFurnace();
 
-        GameRegistry.register(blockOre);
+        registerBlock(blockOre);
         //GameRegistry.register(blockBloomeryFurnce);
+    }
+
+    private static <BLOCK extends Block> BLOCK registerBlock(BLOCK block) {
+        return registerBlock(block, ItemBlock::new);
+    }
+
+    private static <BLOCK extends Block> BLOCK registerBlock(BLOCK block, @Nullable Function<BLOCK, ItemBlock> itemFactory) {
+        GameRegistry.register(block);
+
+        if (itemFactory != null) {
+            final ItemBlock itemBlock = itemFactory.apply(block);
+
+            GameRegistry.register(itemBlock.setRegistryName(block.getRegistryName()));
+        }
+
+        return block;
     }
 }
